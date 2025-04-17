c05c28df816c9ee18f4127fe16e0b6b02abd3d68
SIUUU <token.siuuu@gmail.com>
2025-04-17 21:16:19 +0200

    taille

--- src/sections/layout/NavBar.tsx
+++ src/sections/layout/NavBar.tsx
@@ -45,8 +45,8 @@ export default function NavBar({ darkMode, switchDarkMode }: Props) {
           <Image
             src="/android-chrome-512x512.png"
             alt="Checkmate Tracker logo"
-            width={768}
-            height={512}
+            width={160}
+            height={107}
           />
 
           {/* 
