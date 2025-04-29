6bc8ed3390da524fd96ad96b9efcae5d0fb2a414
SIUUU <token.siuuu@gmail.com>
2025-04-30 00:03:39 +0200

    next config pour export seo

--- next.config.ts
+++ next.config.ts
@@ -1,72 +1,68 @@
 import { withSentryConfig } from "@sentry/nextjs";
 import { NextConfig } from "next";
-import { PHASE_PRODUCTION_BUILD } from "next/constants";
 
-const nextConfig = (phase: string): NextConfig => ({
-  output: phase === PHASE_PRODUCTION_BUILD ? "export" : undefined,
+const nextConfig: NextConfig = {
+  output: "export", // Toujours "export", pas besoin de tester la phase
   trailingSlash: false,
   reactStrictMode: true,
   images: {
     unoptimized: true,
   },
-  headers:
-    phase === PHASE_PRODUCTION_BUILD
-      ? undefined
-      : async () => [
-          {
-            source: "/",
-            headers: [
-              {
-                key: "Cross-Origin-Embedder-Policy",
-                value: "require-corp",
-              },
-              {
-                key: "Cross-Origin-Opener-Policy",
-                value: "same-origin",
-              },
-            ],
-          },
-          {
-            source: "/engines/:blob*",
-            headers: [
-              {
-                key: "Cross-Origin-Embedder-Policy",
-                value: "require-corp",
-              },
-              {
-                key: "Cross-Origin-Opener-Policy",
-                value: "same-origin",
-              },
-            ],
-          },
-          {
-            source: "/play",
-            headers: [
-              {
-                key: "Cross-Origin-Embedder-Policy",
-                value: "require-corp",
-              },
-              {
-                key: "Cross-Origin-Opener-Policy",
-                value: "same-origin",
-              },
-            ],
-          },
-          {
-            source: "/database",
-            headers: [
-              {
-                key: "Cross-Origin-Embedder-Policy",
-                value: "require-corp",
-              },
-              {
-                key: "Cross-Origin-Opener-Policy",
-                value: "same-origin",
-              },
-            ],
-          },
-        ],
-});
+  headers: async () => [
+    {
+      source: "/",
+      headers: [
+        {
+          key: "Cross-Origin-Embedder-Policy",
+          value: "require-corp",
+        },
+        {
+          key: "Cross-Origin-Opener-Policy",
+          value: "same-origin",
+        },
+      ],
+    },
+    {
+      source: "/engines/:blob*",
+      headers: [
+        {
+          key: "Cross-Origin-Embedder-Policy",
+          value: "require-corp",
+        },
+        {
+          key: "Cross-Origin-Opener-Policy",
+          value: "same-origin",
+        },
+      ],
+    },
+    {
+      source: "/play",
+      headers: [
+        {
+          key: "Cross-Origin-Embedder-Policy",
+          value: "require-corp",
+        },
+        {
+          key: "Cross-Origin-Opener-Policy",
+          value: "same-origin",
+        },
+      ],
+    },
+    {
+      source: "/database",
+      headers: [
+        {
+          key: "Cross-Origin-Embedder-Policy",
+          value: "require-corp",
+        },
+        {
+          key: "Cross-Origin-Opener-Policy",
+          value: "same-origin",
+        },
+      ],
+    },
+  ],
+};
 
 export default withSentryConfig(nextConfig, {
   // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
