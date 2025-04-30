a34086e3e7ca7cfd614c4145e95f5d4d93a0e3a0
SIUUU <token.siuuu@gmail.com>
2025-04-30 02:15:46 +0200

    reparation

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,21 +1,17 @@
+// src/pages/_app.tsx
+
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 
 import { AppProps } from "next/app";
-import { DefaultSeo } from "next-seo";
-import SEO from "../../next-seo.config"; // <- ajuste le chemin si besoin
-
 import Layout from "@/sections/layout";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
-      <DefaultSeo {...SEO} />
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </>
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
   );
 }
